26c99d8f214b1e8298b958c76fa6a52519603034
Bro Sand <bsand8813@gmail.com>
2020-02-27 10:05:27 +0800

    Penambahan Tollbar File dan View, Hasil Pertemuan ke 2

--- Pertemuan 1/index.js
+++ Pertemuan 1/index.js
@@ -10,7 +10,7 @@ app.on("ready",  () => {
        webPreferences : {
            nodeIntegration : true
         },
-        title : "Aplikasi Kedokteran"  
+        title : "Aplikasi Dokter"  
     });
 
     todayWindow.loadURL(`file://${__dirname}/today.html`);
@@ -18,4 +18,78 @@ app.on("ready",  () => {
         app.quit();
         todayWindow = null;
     });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
 });
+
+const listWindowCreator = () =>{
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration : true
+        },
+
+        width: 600,
+        height: 400,
+        title : "All Apointments"
+    });
+    
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+
+    listWindow.on("closed", () => (listWindow = null ));
+};
+
+const createWindowCreator = () =>{
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration : true
+        },
+
+        width: 600,
+        height: 400,
+        title : "Create Apointments"
+    });
+    
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+
+    createWindow.on("closed", () => (createtWindow = null ));
+};
+
+const menuTemplate = [{
+    label : "file",
+    submenu : [{
+        label : "New Appointment",
+
+            click(){
+                createWindowCreator();
+            }
+        },
+        {
+            label : "All Apointment",
+            click(){
+                listWindowCreator();
+            }
+        },
+        {
+            label: "Quit",
+            accelerato : process.platform === "darwin" ? "Command+Q" :
+            "Ctrl+Q",
+            click(){
+                app.quit(); 
+            }
+
+
+        }
+
+    ]
+},
+{
+    label: "View",
+    submenu : [{ role : "reload"}, {role: "toggledevtools"}]
+}
+
+]
+
