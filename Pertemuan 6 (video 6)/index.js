db4a8ef7e8568ff5edc0125b0f0d4c600ec31914
Bro Sand <bsand8813@gmail.com>
2020-04-02 12:48:34 +0800

    Menambahkan uuid pada data yang dikirim ke All apointment

---
+++ Pertemuan 6 (video 6)/index.js
@@ -0,0 +1,145 @@
+const electron = require("electron");
+const { v4 : uuidv4 } = require('uuid');
+uuidv4();
+
+const {
+    app, 
+    BrowserWindow, 
+    Menu, 
+    ipcMain
+} = electron;
+
+let todayWindow;
+let createWindow;
+let listWindow;
+let aboutWindow;
+
+let allApointment = [];
+
+app.on("ready", () => {
+    todayWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }, 
+        title : "Aplikasi Kedokteran"
+    });
+
+    todayWindow.loadURL(`file://${__dirname}/today.html`);
+    todayWindow.on("closed", () => {
+
+        app.quit();
+        todayWindow = null;
+    });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow ({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "All Appointments"
+    });
+
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed", ()  => (listWindow = null));
+};
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow ({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Create Appointments"
+    });
+
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("closed", ()  => (createWindow = null));
+};
+
+const aboutWindowCreator = () => {
+    aboutWindow = new BrowserWindow ({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "About"
+    });
+
+    aboutWindow.setMenu(null);
+    aboutWindow.loadURL(`file://${__dirname}/agus.html`);
+    aboutWindow.on("closed", ()  => (caboutWindow = null));
+};
+
+ipcMain.on("appointment:create", (event, appointment) => {
+    appointment["id"] = uuidv4();
+    appointment["done"] = 0;
+    allApointment.push(appointment);
+
+    createWindow.close();
+
+    console.log(allApointment);
+});
+
+ipcMain.on("appointment:request:list", event => {
+    console.log("here1");
+});
+
+ipcMain.on("appointment:request:today", event => {
+    console.log("here2");
+});
+
+ipcMain.on("appointment:done", (event, id) => {
+    console.log("here3");
+});
+
+const menuTemplate =  [{
+        label: "File",
+        submenu:[{
+                label: "New Appointment",
+
+                click() {
+                    createWindowCreator()
+                }
+            },
+            {
+                label: "All Apointments",
+                click() {
+                    listWindowCreator();
+                }
+            },
+            {
+                label: "Quit",
+                accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl + Q",
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    },
+
+    {
+
+        label: "View",
+        submenu: [{ role: "reload" }, { role: "toggledevtools" }]
+    }
+    ,
+    {
+
+        label: "About",
+        click(){
+            aboutWindowCreator();
+}
+    }
+
+
+]
